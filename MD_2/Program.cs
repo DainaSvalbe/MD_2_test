11e0f6bd2f6160d5ae7e9b9fd44166b13dfdff1f
DainaSvalbe <svalbedaina@gmail.com>
2022-04-25 00:33:33 +0300

    min/max

--- MD_2/Program.cs
+++ MD_2/Program.cs
@@ -17,4 +17,18 @@ bool pilngadigs = age1 >= limit;
 Console.WriteLine(pilngadigs);
 
 
+//---------------------------------------------
+
+Console.WriteLine("Min/Max uzdevums ");
+
+Console.WriteLine("Ievadi pirmo skaitli");
+int skaitlis1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ievadi otro skaitli");
+int skaitlis2 = int.Parse(Console.ReadLine());
+
+int maxSkaitlis = Math.Max(skaitlis1, skaitlis2);
+Console.WriteLine("Max skaitlis: " + maxSkaitlis);
+
+int minSkaitlis = Math.Min(skaitlis1, skaitlis2);
+Console.WriteLine($"Min skaitlis: {minSkaitlis}");
 
